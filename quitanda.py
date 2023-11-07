80209b5dff9f39dcaff26b96db16fa64146693a0
Laura Assis Andrela <laura.andrela.senai@gmail.com>
2023-11-07 14:51:42 -0300

    commit para sql e python
    ...

--- quitanda.py
+++ quitanda.py
@@ -3,8 +3,37 @@ import sqlite3 as sql
 import uuid
 
 app = Flask(__name__)
-app.secret_key "quitandazezinho"
+app.secret_key = "quitandazezinho"
 
 usuario = "usuario"
 senha = "senha"
 login = False
+
+def verifica_sessao(): 
+    if "login" in session and session["login"]: 
+        return True
+    else:
+        return False
+
+def conecta_database():
+    conexao = sql.connect("db_quitanda.db")
+    conexao.row_factory = sql.Row
+    return conexao
+
+def iniciar_db():
+    conexao = conecta_database()
+    with app.open_resource('esquema.sql', mode='r') as comandos:
+        conexao.cursor().executescript(comandos.read())
+        conexao.commit()
+        conexao.close
+
+@app.route("/")
+def index():
+    iniciar_db()
+    conexao = conecta_database()
+    produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
+    conexao.close()
+    title = "Home"
+    return render_template("sobre.html", produtos=produtos, title=title)
+
+app.run(debug=True)
